20bc926b24f8fcde00340c0a1780e249471182c1
Mateusz <lazydeveloper@protonmail.com>
2023-09-30 20:51:10 +0200

    first form

--- time_tracker/models.py
+++ time_tracker/models.py
@@ -1,3 +1,10 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class RecordedTime(models.Model):
+    description = models.CharField(max_length=128, null=True, blank=True)
+    project = models.CharField(max_length=64, null=True, blank=True)
+    time = models.IntegerField()
+    billable = models.BooleanField(default=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
--- time_tracker/urls.py
+++ time_tracker/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='websiteIndex'),
+    path('', views.index, name='website_index'),
+    path('create_recorded_time', views.create_recorded_time, name='create_recorded_time'),
 
 ]
